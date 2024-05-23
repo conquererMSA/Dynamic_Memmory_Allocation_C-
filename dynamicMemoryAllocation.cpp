1f4a30a21dee95ee1dd076fb7a7b19cf80c4f8a7
asus test <msa-99@outlook.com>
2024-05-23 23:48:51 +0600

    dynamic array declaration

--- dynamicMemoryAllocation.cpp
+++ dynamicMemoryAllocation.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-void fun(){
+int* fun(){
     int* aPointer=new int;
     //ekahne new keyword diye Heap/Runtime/Dynamic Memory te integer data type er ekta variable create hoyeche. Heap memory te thake sei variable er memory address, static memory te create hoaya aPointer namok variable e sytore ache.
     *aPointer=34;
@@ -9,6 +9,7 @@ void fun(){
     //ekahne aPointer variable er value orthat Heap memory te thaka variable er adrdress return kora hoyeche. return korar sathe sathe static memory te thaka aPointer soho fun function sob kichu delete hoye zabe. kintu Heap memory te create hoa variable ebong er address theke zabe. karon Static/Stack memory return korle auto clear hoye zay. kintu Heap memory auto clear hoy na. User ke delete korte hoy.
 }
 int main(){
-    
+    int* aPointer=fun();
+    cout<<"heap memory address "<<aPointer<<endl<<"value "<<*aPointer<<endl;
     return 0;
 }
